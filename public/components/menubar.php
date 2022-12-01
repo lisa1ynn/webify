86b9f07ca127f1a6a4c45fd30f931e6a66ae8b04
Emil Lystimaki <emilaleksanteri@gmail.com>
2022-12-01 10:17:52 +0000

    freelancer box edits, menu bar as component

---
+++ public/components/menubar.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style>
+        .top-bar-container {
+          background-color: #262626;
+          position: sticky;
+          top: 0;
+          width: 100%;
+          text-align: center;
+          display: grid;
+          grid-template-columns: 1fr 3fr 1fr;
+          z-index: 2;
+          box-shadow: 0px 0.5px 2px black;
+        }
+
+        .logo-bar {
+          width: 30px;
+          height: 30px;
+          padding-top: 14%;
+          margin: 0 auto;
+        }
+
+        .navigation-bar {
+          color: #ffffff;
+        }
+        .navigation-bar li {
+          display: inline-block;
+          padding: 5%;
+          font-size: large;
+        }
+
+        a:link {
+          color: #ffffff;
+          text-decoration: none;
+        }
+
+        a:visited {
+          color: #ffffff;
+        }
+
+        a:hover {
+          color: #7d80ff;
+          text-shadow: 2px 2px 4px black;
+        }
+
+        .login-button {
+          display: inline;
+          padding-top: 2.5rem;
+        }
+        a.login-btn {
+          padding: 3% 10%;
+          border-radius: 30px;
+          width: 60%;
+          background: #ffffff;
+          color: #262626 !important;
+          font-size: large;
+          border: none;
+          box-shadow: 0px 0px 0px black;
+        }
+
+        .login-btn:hover {
+          background: #ffa88e;
+          animation: shadow 0.5s forwards;
+          text-shadow: none;
+        }
+
+        @keyframes shadow {
+          0% {
+            box-shadow: 0px 0px 0px black;
+          }
+          100% {
+            box-shadow: 0px 8px 20px black;
+          }
+        }
+
+        .login-btn:active {
+          background: #ff511c;
+          color: #262626;
+        }
+    </style>
+    </head>
+  <body>
+    <!--menu bar start-->
+    <header class="top-bar-container">
+      <img class="logo-bar" src="" alt="Logo" />
+      <div class="navigation-bar">
+        <li><a class="nav-item" href="#experts">Hire</a></li>
+        <li><a class="nav-item" href="#how works">Purpose</a></li>
+        <li><a class="nav-item" href="#about">About</a></li>
+        <li><a class="nav-item" href="#contact">Contact</a></li>
+      </div>
+      <div class="login-button">
+        <!--Need to just add the onClick function to open login / signup page-->
+        <a href="./sign-in.html" class="login-btn">Login</a>
+      </div>
+    </header>
+    <!--menu bar end-->
+    </body>
+</html>
--- public/mainpage.php
+++ public/mainpage.php
@@ -2,22 +2,25 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title></title>
+    <title>Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="./general.css" />
     <style>
 
       .freelancers-container {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(20rem, 2fr));
         grid-column-gap: 2rem;
-        grid-row-gap: 5rem;
+        grid-row-gap: 2rem;
+        padding-top: 5rem;
       }
 
       .individual-freelancer {
         border-radius: 100px;
         display: flex;
-        justify-self: center;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
       }
 
       .freelancer-tbl-top {
@@ -25,6 +28,9 @@
         background: #262626;
         border: solid;
         border-color: black;
+        display: grid;
+        place-items: center;
+        width: 100%;
       }
 
       .freelancer-tbl-top:hover {
@@ -33,17 +39,28 @@
       }
 
       .freelance-img-projects {
-        width: 300px;
-        height: 200px;
-        border-radius: 100px;
+        width: 350px;
+        height: 250px;
+        border-radius: 90px;
         justify-self: center;
+        border-bottom: solid;
+        border-color: black;
+      }
+
+      .user-name-picture-review-fee {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        width: 90%;
+        padding-bottom: 10%;
+        padding-top: 5%;
       }
 
       .user-p-n {
         display: grid;
-        grid-template-columns: 1fr 2fr;
+        grid-template-columns: 50% 50%;
         padding: 4%;
         justify-self: center;
+        grid-column-gap: 5px;
       }
 
       .profile-p-tbl {
@@ -51,6 +68,7 @@
         height: 40px;
         border-radius: 100%;
         margin: 0 auto;
+        margin-top: 15px;
       }
 
       .usern-tbl {
@@ -59,48 +77,73 @@
         font-weight: bold;
       }
 
-      .reviews {
-        border-radius: 100px;
+      .user-info {
         justify-self: center;
         padding-bottom: 10%;
         padding-left: 15%;
         color: white;
+        font-weight: bold;
+        display: grid;
+        grid-template-rows: 20px 20px;
+        margin-top: 8px;
       }
 
       .review-str {
         width: 15px;
         height: 15px;
-
       }
 
       .review-rating {
         text-align: left;
+      }
+
+      .price {
+        text-align: right;
+        padding-right: 40px;
+      }
+
+      .freelancer-tbl-top:hover > .show-more {
+        text-shadow: 1px 2px 4px black;
+      }
+
+      .hero-mainpg {
+        width: 100%;
+        position: relative;
+        text-align: center;
+        padding-top: 10%;
+        z-index: 1;
+        box-shadow: 0px 0.5px 2px black;
+        padding-bottom: 20px;
+      }     
+
+      .hero-text-small {
+        color: #9a9a9a;
+        padding-top: 20px;
+        text-decoration: underline;
+        text-underline-offset: 1.5rem;
+        letter-spacing: 0.3em;
+        font-family: arial;
+        font-size: small;
+      }
+      h1 {
+        color: #ffffff;
         font-weight: bold;
+        font-size: xx-large;
+        font-family: sans-serif;
+        letter-spacing: 0.05em;
       }
 
     </style>
   </head>
   <body>
     <!--menu bar start-->
-    <header class="top-bar-container">
-      <img class="logo-bar" src="" alt="Logo" />
-      <div class="navigation-bar">
-        <li><a class="nav-item" href="#experts">Hire</a></li>
-        <li><a class="nav-item" href="#how works">Purpose</a></li>
-        <li><a class="nav-item" href="#about">About</a></li>
-        <li><a class="nav-item" href="#contact">Contact</a></li>
-      </div>
-      <div class="login-button">
-        <!--Need to just add the onClick function to open login / signup page-->
-        <a href="./sign-in.html" class="login-btn">Login</a>
-      </div>
-    </header>
+    <?php include './components/menubar.php'; ?>
     <!--menu bar end-->
     <!--Hero element start-->
     <section class="hero-mainpg">
       <!--Span element used to make the gradient text effect-->
       <h1>Welcome to <span class="cool-text">Webify</span></h1>
-      <p class="hero-text-small">For all website freelancing needs.</p>
+      <p class="hero-text-small">Professional Development Re-imagined.</p>
       <!--Tells user to scroll down for more info-->
       <div class="scroll-down-container">
         <p class="scroll-down-arrow">⌄<br />⌄<br />⌄</p>
@@ -108,31 +151,49 @@
     </section>
     <!--Hero end-->
     <section>
+      <div id="filter-section">
+        
+
+      </div>
       <div class="freelancers-container">
-          <?php
-            include '../private/initialize.php';
+        <?php
+          include '../private/initialize.php';?>
+          <?php 
+          // still in the progress of making these work as a filter mechanism based on user filter options
+          $users = $database->query("SELECT * FROM freelancer");
+          $skills_identified = $database->query("SELECT * FROM freelancerskill");
+          $skills = $database->query("SELECT * FROM skills");
+          $filter = array("JavaScript", "PHP");
+          if ("SELECT * FROM skills, freelancerskills AND SELECT id FROM freelancer IF 
+          freelancer.id = freelancerskill.freelancer_id AND
+          freelancerskill.skill_id = skills.skill_id AND
+          skills.skill = 'JavaScript'") {
 
-            $result = $database->query("SELECT fname, lname, intro, skills, offer, fee, points, reviews, projects, profilep FROM freelancer;");
-
-            while($row = $result->fetch_assoc()){
-              $array = $row;
-              $review = number_format($array['points']/$array['reviews'], 2);
-
-             ?>
-            <div class="individual-freelancer" >
-              <div class="freelancer-tbl-top" >
-                <img src="<?php echo 'data:image/png;base64,'.base64_encode($array['projects']).''; ?>" alt="Portfolio Image" class="freelance-img-projects">
+          
+          while($user = $users->fetch_assoc()){
+            $user_row = $user;
+            $review = number_format($user_row['points']/$user_row['reviews'], 2);
+            ?>
+          <div class="individual-freelancer" >
+            <div class="freelancer-tbl-top" >
+              <img src="<?php echo 'data:image/png;base64,'.base64_encode($user_row['projects']).''; ?>" alt="Portfolio Image" class="freelance-img-projects">
+              <div class="user-name-picture-review-fee">
                 <div class="user-p-n">
-                  <img src="<?php echo 'data:image/png;base64,'.base64_encode($array['profilep']).''; ?>" alt="Profile Picture" class="profile-p-tbl">
-                  <p class="usern-tbl"><?php echo $array['fname']." ".$array['lname']; ?></p>
+                  <img src="<?php echo 'data:image/png;base64,'.base64_encode($user_row['profilep']).''; ?>" alt="Profile Picture" class="profile-p-tbl">
+                  <p class="usern-tbl"><?php echo $user_row['fname']." ".$user_row['lname']; ?></p>
                 </div>
-              <div class="reviews">
-                <p class="review-rating"><img src="./pictures/reviewstr.png" alt="Reviews: " class="review-str">  <?php echo $review; ?> </p>
+                <div class="user-info">
+                  <p class="review-rating"> <img src="./pictures/reviewstr.png" alt="Reviews: " class="review-str"> <?php echo $review; ?>/5</p>
+                  <p class="price">Fee: £<?php echo $user_row['fee']; ?> </p>
+                </div>
+              </div>
+              <div class="show-more">
+                <!-- make dots to show image preview of different projects -->
+                  <h1>. . .</h1>
               </div>
             </div>
           </div>
-
-            <?php } mysqli_close($database); ?>
+          <?php }} mysqli_close($database); ?>
       </div>
     </section>
   </body>
