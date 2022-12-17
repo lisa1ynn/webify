731fdaf7779530bd1f43f5549c55a39252b644a6
annaccident <leviosann@outlook.com>
2022-12-17 13:49:07 +0000

    added button at the end of about page

--- public/about.php
+++ public/about.php
@@ -122,7 +122,53 @@
                 text-shadow: 0px 1px 1px black;
                 padding: 100px 15%;
             }
+            
+            /* button design */
+            .getstarted_button {
+                background-color: #7D80FF;
+                color: white;
+                font-family: 'Courier New', Courier, monospace;
+                font-weight: bold;
+                border: solid 2px black;
+                box-shadow: 0px 2px 4px #151515;
+                padding: 15px 32px;
+                text-align: center;
+                display: inline-block;
+                font-size: 2rem;
+                margin: 0;
+                position: absolute;
+                top: 85%;
+                left: 50%;
+                -ms-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%)
+            }
 
+            /* cursor, arrow animation on hover button */
+            button{
+                cursor: pointer;
+                display: inline-block;
+                position: relative;
+                transition: 0.5s;
+            }
+
+            button:after {
+                content: '»';
+                position: absolute;
+                opacity: 0;  
+                top: 14px;
+                right: -20px;
+                transition: 0.5s;
+            }
+
+            button:hover{
+                padding-right: 24px;
+                padding-left:8px;
+            }
+
+            button:hover:after {
+                opacity: 1;
+                right: 10px;
+            }
 
         </style>
     </head>
@@ -159,6 +205,15 @@
                     <p>As a freelancer you will receieve requests in form of forms, visible on your profile tab. If you decide to accept or reject the offer, respond accordingly via email to the user.<br></br>As a user you can browse through our selection of web design services to find the option that best fits your needs. Collaborate with your designer to bring your vision to life. Our designers will work with you to understand your goals and needs, and will use their expertise to create a website that truly reflects your brand and message.</p>
                 </div>
             </div>
+
+            <!-- button to redirect to sign in page -->
+            <div>
+                <button id="startButton" class="getstarted_button"><span>Get Started!</span></button>
+                <script type="text/javascript">
+                    document.getElementById("startButton").onclick = function () {
+                    location.href = "http://localhost/webify/public/sign-in.php";
+                };
+            </script>
 
             
         </div>
